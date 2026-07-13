7b2630f9e21d75bb8789ec30966212e08c7a9bae
vicente <pinarojedavicente@gmail.com>
2026-07-13 19:40:13 +0200

    My first upload of self-learned skills

---
+++ JavaScript/Ejemplo1/index.js
@@ -0,0 +1,18 @@
+console.log(`Hello`);
+console.log(`I am Vicen`);
+
+//window.alert(`This is an alert`);
+//window.alert(`Be careful`);
+
+document.getElementById("myH1").textContent = `HELLO :)`;
+
+document.getElementById("myP").textContent = `This is my first website using JavaScript`;
+//this is a comment
+
+/*
+    this 
+    is 
+    a 
+    multi-line 
+    comment
+*/
