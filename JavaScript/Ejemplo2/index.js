1dd28997d5f26a1277748cd968518a9d62edc5d1
vicente <pinarojedavicente@gmail.com>
2026-07-13 20:23:18 +0200

    Uploading another exercise

---
+++ JavaScript/Ejemplo2/index.js
@@ -0,0 +1,35 @@
+//variables
+/*
+//Number variables
+let age = 120;
+
+console.log(typeof age);
+console.log(`I am ${age} years old`);
+console.log(`Im joking`);
+
+
+//String variables
+let firstName = "Vicente";
+let food = "pizza";
+
+console.log(typeof firstName);
+console.log(`My name is ${firstName}`);
+console.log(`Do you like ${food}?`);
+
+//boolean variables
+
+let online = false;
+let likesPizza = true;
+
+console.log(typeof online);
+console.log(`${firstName} is online: ${online}`);
+console.log(`Does ${firstName} like pizza? ${likesPizza}`);
+*/
+
+let name = "Vin";
+let age = "18";
+let isStudent = true;
+
+document.getElementById("p1").textContent = `My name is ${name}`;
+document.getElementById("p2").textContent = `I am ${age} years old`;
+document.getElementById("p3").textContent = `Student: ${isStudent}`;
