2f5cab0760166f49c452892119bce1397bbc8ca5
vicente <pinarojedavicente@gmail.com>
2026-07-20 20:10:06 +0200

    New games/programs

---
+++ JavaScript/DiceRoller/index.js
@@ -0,0 +1,22 @@
+//Dice roller program
+
+
+
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src = img/dice${value}.png alt= "Dice ${value}">`)
+    }
+
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
+
+}
+
